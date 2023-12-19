a6d95258b2a2cbc4366efb8557400edf25b6d118
ydmins <dr.ydmins@gmail.com>
2023-12-19 15:25:40 +0900

    studied c++ and practiced the program codelite

---
+++ C++/01_Getting_Started/Workspace4/Workspace4/BasicIO/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello world" << endl;
+    
+    cout << "Hello world";
+    
+    cout << "Hello";
+    cout << "world" << endl;
+
+
+    return 0;
+}
