071f75cbd6bca8fb8f9a608d316364e0dafa5f0c
ydmins <dr.ydmins@gmail.com>
2024-01-03 20:18:19 +0900

    studied C++ last time and added recipe model

--- Angular/02-course-project/src/app/recipes/recipes-list/recipes-list.component.ts
+++ Angular/02-course-project/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrl: './recipes-list.component.css'
 })
-export class RecipesListComponent {
+export class RecipesListComponent implements OnInit{
+  recipes : Recipe[]= [
+    new Recipe('A Test Recipe', 'This is simply a recipe.', 'https://www.southernliving.com/thmb/HSEUOjJVCl4kIRJRMAZ1eblQlWE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg')
+  ];
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
 
 }
