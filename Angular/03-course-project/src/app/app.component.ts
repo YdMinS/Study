226ad1c36b505abf63e765ada50e004a7e3fbe1d
ydmins <dr.ydmins@gmail.com>
2024-01-08 20:32:20 +0900

    practiced to use hook methods in terms of lifecycle

--- Angular/03-course-project/src/app/app.component.ts
+++ Angular/03-course-project/src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent {
       type: 'server',
       name: serverData.serverName,
       content: serverData.serverContent
-  })
+  });
   }
   
   onBlueprintAdded(blueprintData:{serverName: string, serverContent: string}){
@@ -21,7 +21,13 @@ export class AppComponent {
       type: 'blueprint',
       name: blueprintData.serverName,
       content: blueprintData.serverContent
-    })
+    });
+  }
+  onChangeFirst(){
+    this.serverElements[0].name = 'Changed';
+  }
 
+  onDestroyFirst(){
+    this.serverElements.splice(0,1);
   }
 }
