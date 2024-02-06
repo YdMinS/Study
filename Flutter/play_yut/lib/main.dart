ffab88da5332c3fbf5098cacf33861ca0071d33c
ydmins <dr.ydmins@gmail.com>
2024-02-06 17:02:40 +0900

    set the background image

--- Flutter/play_yut/lib/main.dart
+++ Flutter/play_yut/lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(
+    MaterialApp(
+      home: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage('assets/elements/background_image.jpg'),
+          ),
+        ),
+        child: const Scaffold(
+          backgroundColor: Colors.transparent,
+        ),
+      ),
+    ),
+  );
 }
