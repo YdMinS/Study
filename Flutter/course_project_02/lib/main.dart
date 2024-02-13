30bf833ee6065840bae75d9ed8835ab9c1f4322e
ydmins <dr.ydmins@gmail.com>
2024-02-13 21:02:35 +0900

    flutter - building two pages

---
+++ Flutter/course_project_02/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:course_project_02/quiz.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Quiz());
+}
---
+++ Flutter/course_project_02/lib/quiz.dart
@@ -0,0 +1,51 @@
+import 'package:course_project_02/questions_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'package:course_project_02/start_screen.dart';
+import 'package:course_project_02/questions_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 255, 110, 0),
+                Color.fromARGB(255, 196, 255, 0),
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
