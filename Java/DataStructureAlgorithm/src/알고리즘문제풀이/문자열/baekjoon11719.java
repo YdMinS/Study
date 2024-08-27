a21386e506689c21b0b53facfc57cec42e5720fa
ydmins <dr.ydmins@gmail.com>
2024-08-27 09:07:28 +0900

    Algorithm : Solved Baekjoon 11719

---
+++ Java/DataStructureAlgorithm/src/알고리즘문제풀이/문자열/baekjoon11719.java
@@ -0,0 +1,15 @@
+package 알고리즘문제풀이.문자열;
+
+import java.lang.*;
+import java.io.*;
+
+public class baekjoon11719 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        String str;
+        while((str=br.readLine())!=null){
+            System.out.println(str);
+        }
+    }
+}
