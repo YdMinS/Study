e5fb366fda90aebd6dc06df377cb35899e9685b1
ydmins <dr.ydmins@gmail.com>
2024-09-10 09:25:15 +0900

    Algorithm : Solved Baekjoon 18108

---
+++ Java/DataStructureAlgorithm/src/알고리즘문제풀이/문자열/baekjoon18108.java
@@ -0,0 +1,11 @@
+package 알고리즘문제풀이.문자열;
+
+import java.io.*;
+
+public class baekjoon18108 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        System.out.println(a-543);
+    }
+}
