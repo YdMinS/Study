e442528c970ca61fd44d78562d862095bb732518
ydmins <dr.ydmins@gmail.com>
2024-09-10 09:19:15 +0900

    Algorithm : Solved Baekjoon 10926

---
+++ Java/DataStructureAlgorithm/src/알고리즘문제풀이/문자열/baekjoon10926.java
@@ -0,0 +1,12 @@
+package 알고리즘문제풀이.문자열;
+
+import java.lang.*;
+import java.io.*;
+
+public class baekjoon10926 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(str+"??!");
+    }
+}
