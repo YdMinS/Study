2464beb010974a1ce72f6d1fc16e6992df2d4e98
ydmins <dr.ydmins@gmail.com>
2024-09-21 11:02:45 +0900

    Algorithm : Solved Baekjoon 2426

---
+++ Java/DataStructureAlgorithm/src/알고리즘문제풀이/빅오/baekjoon2426.java
@@ -0,0 +1,10 @@
+package 알고리즘문제풀이.빅오;
+
+import java.lang.*;
+import java.io.*;
+
+public class baekjoon2426 {
+    public static void main(String[] args) throws IOException{
+        System.out.println(1+"\n"+0);
+    }
+}
