14eb56df80e3517c3e729825cd49059d5b55fa53
ydmins <dr.ydmins@gmail.com>
2024-09-24 12:47:47 +0900

    Algorithm : Solved Baekjoon 5338

---
+++ Java/DataStructureAlgorithm/src/알고리즘문제풀이/시뮬레이션_구현/baekjoon5338.java
@@ -0,0 +1,13 @@
+package 알고리즘문제풀이.시뮬레이션_구현;
+
+public class baekjoon5338 {
+    public static void main(String[] args) {
+        StringBuilder strBuilder = new StringBuilder();
+        strBuilder.append("       _.-;;-._ \n");
+        strBuilder.append("'-..-'|   ||   | \n");
+        strBuilder.append("'-..-'|_.-;;-._|\n");
+        strBuilder.append("'-..-'|   ||   |\n");
+        strBuilder.append("'-..-'|_.-''-._|");
+        System.out.println(strBuilder);
+    }
+}
