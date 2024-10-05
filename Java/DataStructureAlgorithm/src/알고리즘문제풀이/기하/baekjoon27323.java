78c866c2a1105b4cb06306233b68b0d6f15e0632
ydmins <dr.ydmins@gmail.com>
2024-10-05 18:55:27 +0900

    Algorithm : Solved Baekjoon 27323

---
+++ Java/DataStructureAlgorithm/src/알고리즘문제풀이/기하/baekjoon27323.java
@@ -0,0 +1,13 @@
+package 알고리즘문제풀이.기하;
+
+import java.lang.*;
+import java.io.*;
+
+public class baekjoon27323 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        System.out.println(A*B);
+    }
+}
