e33b195eece7330864bce089d0d3261abc494585
ydmins <dr.ydmins@gmail.com>
2024-10-06 16:40:20 +0900

    Algorithm : Solved Baekjoon 15894

---
+++ Java/DataStructureAlgorithm/src/알고리즘문제풀이/기하/baekjoon15894.java
@@ -0,0 +1,12 @@
+package 알고리즘문제풀이.기하;
+
+import java.lang.*;
+import java.io.*;
+
+public class baekjoon15894 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Long.parseLong(br.readLine());
+        System.out.println(4*N);
+    }
+}
