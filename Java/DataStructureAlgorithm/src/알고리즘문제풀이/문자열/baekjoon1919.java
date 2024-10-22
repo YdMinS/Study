c9f7f13e03916aaba79973dbf7ed8f4db815b431
ydmins <dr.ydmins@gmail.com>
2024-10-22 11:20:47 +0900

    Algorithm : Moved Baekjoon 1919 to an other directory

--- Java/DataStructureAlgorithm/src/알고리즘문제풀이/문자열/baekjoon1919.java
+++ Java/DataStructureAlgorithm/src/알고리즘문제풀이/문자열/baekjoon1919.java
@@ -1,10 +1,10 @@
-package 알고리즘문제풀이.기하;
+package 알고리즘문제풀이.문자열;
 
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-public class baekjoon3053 {
+public class baekjoon1919 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
