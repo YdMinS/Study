9cd2c5b70e762f462e193f096c9b8abe18970b36
ydmins <dr.ydmins@gmail.com>
2024-11-18 16:28:57 +0900

    add two classes Member and Team and edit Main.java

--- Java/JPA/JPQL/src/main/java/com/ydmins/Main.java
+++ Java/JPA/JPQL/src/main/java/com/ydmins/Main.java
@@ -1,7 +1,27 @@
 package com.ydmins;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
     }
 }
