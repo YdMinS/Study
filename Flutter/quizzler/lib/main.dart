93559863c9b96b4e1ea488cc20f04fd192f535f9
ydmins <dr.ydmins@gmail.com>
2024-12-19 16:19:32 +0900

    developt the quiz app applying with rflutter_aller

--- Flutter/quizzler/lib/main.dart
+++ Flutter/quizzler/lib/main.dart
@@ -1,17 +1,148 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Quizzler());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Quizzler extends StatelessWidget {
+  const Quizzler({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+            child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10.0),
+          child: QuizPage(),
+        )),
+      ),
     );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [];
+
+  void checkAnswer(bool userPickedAnswer) {
+    if (quizBrain.hasNextQuestion()) {
+      bool correctAnswer = quizBrain.getCorrectAnswer();
+      setState(() {
+        if (correctAnswer == userPickedAnswer) {
+          scoreKeeper.add(Icon(
+            Icons.check,
+            color: Colors.green,
+          ));
+        } else {
+          scoreKeeper.add(Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+        }
+        quizBrain.nextQuestion();
+      });
+    } else {
+      _onBasicAlertPressed(context);
+      setState(() {
+        quizBrain.initQuestionNumber();
+        scoreKeeper = [];
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionTest(),
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 25.0, color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: InkWell(
+              onTap: () {
+                checkAnswer(true);
+              },
+              child: Container(
+                color: Colors.green,
+                child: Center(
+                  child: Text(
+                    'True',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: InkWell(
+              onTap: () {
+                checkAnswer(false);
+              },
+              child: Container(
+                color: Colors.red,
+                child: Center(
+                  child: Text(
+                    'False',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Row(
+          children: scoreKeeper,
+        ),
+      ],
+    );
+  }
+
+  _onBasicAlertPressed(BuildContext context) {
+    Alert(
+      context: context,
+      title: "QUIZZLER ALERT",
+      desc: "All quiz's are solved.",
+      buttons: [
+        DialogButton(
+          child: Text("OK"),
+          onPressed: () => Navigator.pop(context),
+        )
+      ],
+    ).show();
   }
 }
---
+++ Flutter/quizzler/lib/quiz_brain.dart
@@ -0,0 +1,58 @@
+import 'question.dart';
+
+class QuizBrain {
+  int _questionNumber = 0;
+
+  final List<Question> _questionBank = [
+    Question('1. Some cats are actually allergic to humans', true),
+    Question('2. You can lead a cow down stairs but not up stairs.', false),
+    Question(
+        '3. Approximately one quarter of human bones are in the feet.', true),
+    Question('4. A slug\'s blood is green.', true),
+    Question('5. Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
+    Question('6. It is illegal to pee in the Ocean in Portugal.', true),
+    Question(
+        '7. No piece of square dry paper can be folded in half more than 7 times.',
+        false),
+    Question(
+        '8. In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        true),
+    Question(
+        '9. The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        false),
+    Question(
+        '10. The total surface area of two human lungs is approximately 70 square metres.',
+        true),
+    Question('11. Google was originally called \"Backrub\".', true),
+    Question(
+        '12. Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        true),
+    Question(
+        '13. In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        true),
+  ];
+
+  void nextQuestion() {
+    if (_questionNumber < _questionBank.length - 1) {
+      _questionNumber++;
+    } else {
+      _questionNumber = 0;
+    }
+  }
+
+  bool hasNextQuestion() {
+    return _questionNumber < _questionBank.length - 1;
+  }
+
+  void initQuestionNumber() {
+    _questionNumber = 0;
+  }
+
+  String getQuestionTest() {
+    return _questionBank[_questionNumber].questionText;
+  }
+
+  bool getCorrectAnswer() {
+    return _questionBank[_questionNumber].questionAnswer;
+  }
+}
