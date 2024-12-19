ea0a0bcfdd8e571802947a0937bd3ed1345a598a
ydmins <dr.ydmins@gmail.com>
2024-12-19 19:01:24 +0900

    completed toy project

--- Flutter/Destini/lib/main.dart
+++ Flutter/Destini/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'story_brain.dart';
+
+StoryBrain storyBrain = StoryBrain();
 
 void main() {
   runApp(const Destini());
@@ -22,64 +25,86 @@ class Destini extends StatelessWidget {
           body: SafeArea(
             child: Padding(
               padding: const EdgeInsets.all(10.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Expanded(
-                    flex: 5,
-                    child: Center(
-                      child: Text(
-                        'Text',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 25.0, color: Colors.white),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.all(15.0),
-                      child: InkWell(
-                        onTap: () {
-                          print('Yes is tapped');
-                        },
-                        child: Container(
-                          color: Colors.red,
-                          child: Center(
-                              child: Text(
-                            'Yes',
-                            style:
-                                TextStyle(fontSize: 25.0, color: Colors.white),
-                          )),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.all(15.0),
-                      child: GestureDetector(
-                        onTap: () {
-                          print('No is tapped');
-                        },
-                        child: Container(
-                          color: Colors.blue,
-                          child: Center(
-                              child: Text(
-                            'No',
-                            style:
-                                TextStyle(fontSize: 25.0, color: Colors.white),
-                          )),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              child: Story(),
             ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class Story extends StatefulWidget {
+  const Story({super.key});
+
+  @override
+  State<Story> createState() => _StoryState();
+}
+
+class _StoryState extends State<Story> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Center(
+            child: Text(
+              storyBrain.getStory(),
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 25.0, color: Colors.white),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: InkWell(
+              onTap: () {
+                print('Yes is tapped');
+                setState(() {
+                  storyBrain.getChoice1();
+                });
+              },
+              child: Container(
+                color: Colors.red,
+                child: Center(
+                    child: Text(
+                  'Yes',
+                  style: TextStyle(fontSize: 25.0, color: Colors.white),
+                )),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: GestureDetector(
+              onTap: () {
+                print('No is tapped');
+                setState(() {
+                  storyBrain.getChoice2();
+                });
+              },
+              child: Visibility(
+                visible: storyBrain.buttonShouldBeVisible(),
+                child: Container(
+                  color: Colors.blue,
+                  child: Center(
+                    child: Text(
+                      'No',
+                      style: TextStyle(fontSize: 25.0, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
