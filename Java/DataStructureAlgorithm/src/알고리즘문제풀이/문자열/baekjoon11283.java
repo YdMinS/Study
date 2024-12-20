c62b8be6dee0ff142179ef718484a84401859cf8
ydmins <dr.ydmins@gmail.com>
2024-12-20 10:40:47 +0900

    solved baekjoon 11283

---
+++ Java/DataStructureAlgorithm/src/알고리즘문제풀이/문자열/baekjoon11283.java
@@ -0,0 +1,10 @@
+package 알고리즘문제풀이.문자열;
+
+import java.io.*;
+
+public class baekjoon11283 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println((int)(br.readLine().charAt(0)-'가')+1);
+    }
+}
