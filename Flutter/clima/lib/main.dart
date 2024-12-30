e997e2a3852a8bd5dfb0031352a82121e7194220
ydmins <dr.ydmins@gmail.com>
2024-12-30 16:05:04 +0900

    feat : added loading_screen.dart

--- Flutter/clima/lib/main.dart
+++ Flutter/clima/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/loading_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
+      home: LoadingScreen(),
     );
   }
 }
