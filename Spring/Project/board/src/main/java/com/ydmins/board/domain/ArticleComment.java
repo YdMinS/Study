740fc25ba5f3cbc3addbd11eddaed94364bb84c2
ydmins <dr.ydmins@gmail.com>
2025-01-07 19:28:48 +0900

    refactor : extract auditing fields

--- Spring/Project/board/src/main/java/com/ydmins/board/domain/ArticleComment.java
+++ Spring/Project/board/src/main/java/com/ydmins/board/domain/ArticleComment.java
@@ -22,7 +22,7 @@ import java.util.Objects;
 })
 @EntityListeners(AuditingEntityListener.class)
 @Entity
-public class ArticleComment {
+public class ArticleComment extends AuditingFields {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,19 +34,6 @@ public class ArticleComment {
     @Setter
     @Column(nullable = false, length = 500)
     private String content;
-
-    @CreatedDate
-    @Column(nullable = false)
-    private LocalDateTime createdAt;
-    @CreatedBy
-    @Column(nullable = false, length = 100)
-    private String createdBy;
-    @LastModifiedDate
-    @Column(nullable = false)
-    private LocalDateTime modifiedAt;
-    @LastModifiedBy
-    @Column(nullable = false, length = 100)
-    private String modifiedBy;
 
     protected ArticleComment() {}
 
