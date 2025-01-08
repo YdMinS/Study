c64365422622e05cd77af2cb03254c2b3890d164
ydmins <dr.ydmins@gmail.com>
2025-01-08 13:36:59 +0900

    sol29701aekjoon 2857

---
+++ Java/DataStructureAlgorithm/src/알고리즘문제풀이/문자열/baekjoon2857.java
@@ -0,0 +1,18 @@
+package 알고리즘문제풀이.문자열;
+
+import java.io.*;
+
+public class baekjoon2857 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder strBuilder = new StringBuilder();
+        for(int i=1; i<=5; i++){
+            String str = br.readLine().toUpperCase();
+            if (str.contains("FBI")) {
+                strBuilder.append(i);
+            }
+        }
+        String str = strBuilder.toString();
+        System.out.println(str.length()==0 ? "HE GOT AWAY!" : str);
+    }
+}
