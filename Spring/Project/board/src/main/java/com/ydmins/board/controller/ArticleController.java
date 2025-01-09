154e5ff7f68d146b1b7f7e6a0311eb1058658095
ydmins <dr.ydmins@gmail.com>
2025-01-09 18:31:50 +0900

    feat: added ArticleController and test code of it - only success case

---
+++ Spring/Project/board/src/main/java/com/ydmins/board/controller/ArticleController.java
@@ -0,0 +1,10 @@
+package com.ydmins.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/v1/articles")
+@Controller
+public class ArticleController {
+
+}
---
+++ Spring/Project/board/src/test/java/com/ydmins/board/controller/ArticleControllerTest.java
@@ -0,0 +1,72 @@
+package com.ydmins.board.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View Controller - Articles")
+@WebMvcTest(controllers = ArticleControllerTest.class)
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] Article List - success")
+    @Test
+    public void givenNothing_whenRequestingArticlesView_thenReturningArticlesView() throws Exception{
+        // given
+
+        // when
+        mvc.perform(get("/v1/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+        // then
+    }
+
+    @DisplayName("[view][GET] Article Retrieve - success")
+    @Test
+    public void givenNothing_whenRequestingArticleView_thenReturningArticleView() throws Exception{
+        // given
+
+        // when
+        mvc.perform(get("/v1/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("article"));
+        // then
+    }
+
+    @DisplayName("[view][GET] Article Search Page - success")
+    @Test
+    public void givenNothing_whenRequestingArticlesSearchView_thenReturningArticlesSearchView() throws Exception{
+        // given
+
+        // when
+        mvc.perform(get("/v1/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+        // then
+    }
+
+    @DisplayName("[view][GET] Article Search with Hashtags - success")
+    @Test
+    public void givenNothing_whenRequestingArticlesHashtagSearchView_thenReturningHashtagArticlesSearchView() throws Exception{
+        // given
+
+        // when
+        mvc.perform(get("/v1/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+        // then
+    }
+}
