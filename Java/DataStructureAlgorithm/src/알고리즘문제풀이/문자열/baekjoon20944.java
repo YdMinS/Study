2af62ffcf8838e12ee449548ec3b4bcbc9b0c8c1
ydmins <dr.ydmins@gmail.com>
2025-01-22 13:01:55 +0900

    solved baekjoon 20944

---
+++ Java/DataStructureAlgorithm/src/알고리즘문제풀이/문자열/baekjoon20944.java
@@ -0,0 +1,11 @@
+package 알고리즘문제풀이.문자열;
+
+import java.io.*;
+
+public class baekjoon20944 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println("a".repeat(N));
+    }
+}
