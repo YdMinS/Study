3cd294632565372c2f3b600d40c15f10ee36cb55
ydmins <dr.ydmins@gmail.com>
2025-02-04 20:34:48 +0900

    solved baekjoon 32652

---
+++ Java/DataStructureAlgorithm/src/알고리즘문제풀이/문자열/baekjoon32652.java
@@ -0,0 +1,11 @@
+package 알고리즘문제풀이.문자열;
+
+import java.io.*;
+
+public class baekjoon32652 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println("AKAR".repeat(N)+"AKA");
+    }
+}
