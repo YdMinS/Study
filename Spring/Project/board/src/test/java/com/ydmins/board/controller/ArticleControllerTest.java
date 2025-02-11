8149cb84ada4bded23a4effc86409b0bd19efdab
ydmins <dr.ydmins@gmail.com>
2025-02-11 20:22:07 +0900

    시큐리티 설정을 모든 요청에 인증이 열리도록 수정

--- Spring/Project/board/src/test/java/com/ydmins/board/controller/ArticleControllerTest.java
+++ Spring/Project/board/src/test/java/com/ydmins/board/controller/ArticleControllerTest.java
@@ -1,16 +1,19 @@
 package com.ydmins.board.controller;
 
+import com.ydmins.board.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@Import(SecurityConfig.class)
 @WebMvcTest(controllers = ArticleController.class)
 class ArticleControllerTest {
 
