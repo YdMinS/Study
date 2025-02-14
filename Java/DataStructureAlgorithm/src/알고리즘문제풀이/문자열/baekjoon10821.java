16e871a7c8a98766e6c386f781dbc77681bb8d76
ydmins <dr.ydmins@gmail.com>
2025-02-14 12:24:19 +0900

    solved baekjoon 10821

---
+++ Java/DataStructureAlgorithm/src/알고리즘문제풀이/문자열/baekjoon10821.java
@@ -0,0 +1,11 @@
+package 알고리즘문제풀이.문자열;
+
+import java.io.*;
+
+public class baekjoon10821 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] numArr = br.readLine().split(",");
+        System.out.println(numArr.length);
+    }
+}
